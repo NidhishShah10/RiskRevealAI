5d47c6a08c6cc3ef14127a8b88f3943c57cc1604
Nidhish Shah <Nidhish.shah@hotmail.com>
2026-05-14 08:37:42 +0530

    Organised Project Files

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse, HTMLResponse
+from fastapi.staticfiles import StaticFiles
+import requests
+import os 
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+app = FastAPI("Risk Reveal AI")
+templates = Jinja2Templates(directory="templates")
+
+st.subheader("Smart Phishing Detection Assistant")
+
+message = st.text_area("Paste Email or Message")
+
+if st.button("Analyze"):
+    st.write("Analyzing message...")
